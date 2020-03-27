ae6120625eb1fbbf5a50da40a5ba2340dedf8f18
Esvin Ambrocio <esvinambrocio@localhost.localdomain>
2020-03-27 17:52:30 -0600

    lesson 6 autoload and namespaces styde.net

--- phpOop/inheritance.php
+++ phpOop/inheritance.php
@@ -1,108 +1,24 @@
 <?php
-    function show($message){
-        echo "<p>$message</p>";
-    }
-    abstract Class Unit{
-        protected $hp = 40;
-        protected $name;
+
+    namespace Styde;
+
+    spl_autoload_register(function($className){
         
-        public function __construct($name){
-            $this->name = $name;
-        }
-
-        public function getHp(){
-            return $this->hp;
-        }
-
-        public function getName(){
-            return $this->name;
-        }
-
-        public function move($direction){
-            show("{$this->name} avanza hacia $direction");
-        }
-
-        abstract public function attack(Unit $opponent);
-
-        public function takeDamage($damage){
+        if(strpos($className,'Styde\\') == 0){
             
-            $this->hp = $this->hp - $this->absorbDamage($damage);
-            show("{$this->name} ahora tiene {$this->hp} puntos de vida");
-        
-            if($this->hp <= 0){
-                $this->die();
-            } 
-        }
-        public function die(){
-            show("{$this->name} muere");
-            exit();
-        }
-
-        protected function absorbDamage($damage){
-            return $damage;
-        }
-    };
-    class Soldier extends Unit{
-        protected $damage = 40;
-        protected $armor;
-
-        public function __construct($name){
-            parent::__construct($name);
-        }
-
-        public function setarmor(Armor $armor = null){
-            $this->armor = $armor;
-        }
-
-        public function attack(Unit $opponent){
-            show("{$this->name} ataca con la espada a {$opponent->getName()}");
-            $opponent->takeDamage($this->damage);
-        }
-/*         public function takeDamage($damage){
+            $className = str_replace('Styde\\', '',$className);
             
-            $damage = $this->absorbDamage($damage);
+            if(file_exists("src/$className.php")){
+                require "src/$className.php";
+            };
 
-            return parent::takeDamage($damage);
-        } */
-        protected function absorbDamage($damage){
-            if($this->armor){
-                $damage = $this->armor->absorbDamage($damage);
-            }
-            return $damage;
         }
-    };
-    class Archer extends Unit{
-        protected $damage = 20;
+    });
 
-        public function attack(Unit $opponent){
-            show("{$this->name} dispara una flecha a {$opponent->getName()}");
-            $opponent->takeDamage($this->damage);             
-        }
-    };
+    require 'vendor/Armor.php';
+    require './src/helpers.php';
 
-    interface Armor{
-        public function absorbDamage($damage);
-    }
-
-    class BronceArmor implements Armor{
-        public function absorbDamage($damage){
-            return $damage/2;
-        }
-    };
-
-    class SilverArmor implements Armor{
-        public function absorbDamage($damage){
-            return $damage/3;
-        }
-    };
-
-    class CursedArmor implements Armor{
-        public function absorbDamage($damage){
-            return $damage*2;
-        }
-    }
-
-    $armor = new SilverArmor();
+    $armor = new BronzeArmor();
     $ramm = new Soldier("Ramm");
 
     $silence = new Archer("Silence");
---
+++ phpOop/src/BronzeArmor.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Styde;
+use Warcraft\Armor;
+
+class BronzeArmor implements Armor{
+    public function absorbDamage($damage){
+        return $damage/2;
+    }
+};
